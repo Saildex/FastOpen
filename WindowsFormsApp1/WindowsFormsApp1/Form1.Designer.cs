360bb798984956b1728e7f0c24d36a896f083126
Saildex <69634762+Saildex@users.noreply.github.com>
2021-06-18 18:22:41 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.Tag = "dsada";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            resources.GetString("textBox1.AutoCompleteCustomSource")});
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System.IO;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;                         
+using System.Text;                                                               
+using System.Threading.Tasks;              
+using System.Windows.Forms;                
+using System.Diagnostics;                  
+using System;                              
+
+namespace WindowsFormsApp1
+{
+
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex;
+            
+            string error = "The program was not found at the specified path in the code, if you chose MyApp, Try to contact the author of the program Master pro 1322#5500";
+            string name = Environment.UserName;
+
+            string text = textBox1.Text;
+
+            
+
+            try
+            {
+                switch (index)
+                {
+                    
+                    case 0: Process.Start("taskmgr.exe"); break;
+                    case 1: Process.Start("Notepad.exe"); break;
+                    case 2: Process.Start(@"D:\Minecraft\MinecraftLauncher.exe"); break;
+                    case 3: Process.Start(@"C:\Users\{name}\AppData\Roaming\Spotify\Spotify.exe"); break;
+                    case 4: label2.Visible = false; label1.Visible = true; textBox1.Visible = true; Process.Start(text); break;
+                    default: MessageBox.Show("You have not selected an application"); break;
+                }
+            }
+
+            catch { MessageBox.Show(error); }
+        }
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    }
+
+
+
+//case номер кейса:
+//                    try
+//{
+//    Process.Start("Путь к программе");
+//}
+//catch
+//{
+//    MessageBox.Show("Программа не найдена по указанному пути в коде" +
+//"\nПопробуйте связаться с автором программы Master pro 1322#5500");
+//}
+//break;
+//При добавлении программы добавить коллекцию в комбобокс1 
+
+    //СТАРАЯ ВЕРСИЯ !!!!! 18.06.2021
+//try
+//            {
+//                switch (index)
+//                {
+
+//                    case 0:
+
+
+//                        Process.Start("taskmgr.exe");
+
+//                        break;
+//                    case 1:
+
+//                        Process.Start("Notepad.exe");
+
+//                        break;
+//                    case 2:
+
+//                        Process.Start("D:\\Minecraft\\MinecraftLauncher.exe");
+
+//                        break;
+//                    case 3:
+
+//                        Process.Start("C:\\Users\\Арслан\\AppData\\Roaming\\Spotify\\Spotify.exe");
+//                        break;
+//                    case 4:
+
+//                        string pathtoapp = textBox1.Lines[2];
+//                        Process.Start(pathtoapp);
+                        
+//                        break;
+//                }
+//            }
+//            catch
+//{
+//    MessageBox.Show("Программа не найдена по указанному пути в коде" +
+//"\nПопробуйте связаться с автором программы Master pro 1322#5500");
+//}
+//        } 
